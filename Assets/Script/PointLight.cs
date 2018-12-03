0b1309165da22aba99395a5c2f08166d10f4dd61
CurryPseudo <jesonxiake@126.com>
2018-12-03 22:58:58 +0800

    作为child gameObject动态管理light mesh

--- Assets/Script/PointLight.cs
+++ Assets/Script/PointLight.cs
@@ -2,25 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
 [ExecuteInEditMode]
 public class PointLight : MonoBehaviour {
 	public float radius;
 	public int triangleCount;
 	public Color lightColor;
-	private MeshFilter meshFilter;
-	private Mesh mesh;
-	void Awake() {
-		meshFilter = GetComponent<MeshFilter>();
-	}
-	void Update() {
-		if(mesh == null) {
-			mesh = new Mesh();
+	public Material lightMaterial;
+	[System.Serializable]
+	public struct MeshRenderInfo {
+		public MeshFilter filter;
+		public Mesh mesh;
+
+		public void Setup(PointLight pointLight, string name, Material material) {
+			if(filter == null) {
+				GameObject go = new GameObject(name);
+				go.transform.SetParent(pointLight.transform, false);
+				filter = go.AddComponent<MeshFilter>();
+				var renderer = go.AddComponent<MeshRenderer>();
+				renderer.material = material;
+				mesh = new Mesh();
+				filter.mesh = mesh;
+			}
 		}
+    }
+	public MeshRenderInfo light = new MeshRenderInfo();
+	void Update() {
+		light.Setup(this, "LightMesh", lightMaterial);
 		if(triangleCount < 3) {
 			triangleCount = 3;
 		}
-		mesh.Clear();
+		light.mesh.Clear();
 		Vector3[] vertices = new Vector3[triangleCount + 2];
 		vertices[0] = Vector3.zero;
 		float stepAngle = Mathf.PI * 2 / triangleCount;
@@ -45,12 +56,11 @@ public class PointLight : MonoBehaviour {
 		for(int i = 0; i < triangleCount + 1; i++) {
 			colors[i + 1] = Color.black;
 		}
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-		mesh.colors = colors;
-		mesh.RecalculateNormals();
-		meshFilter.mesh = mesh;
+		light.mesh.vertices = vertices;
+		light.mesh.triangles = triangles;
+		light.mesh.colors = colors;
+		light.mesh.RecalculateNormals();
+		light.filter.mesh = light.mesh;
 
-		var meshRenderer = GetComponent<MeshRenderer>();
 	}
 }
