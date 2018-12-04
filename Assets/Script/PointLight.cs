9eea129e6042c2301d0dfb13d822d12a757c46a5
CurryPseudo <jesonxiake@126.com>
2018-12-04 12:49:19 +0800

    Shadow mesh with problem

--- Assets/Script/PointLight.cs
+++ Assets/Script/PointLight.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,8 @@ public class PointLight : MonoBehaviour {
 	public int triangleCount;
 	public Color lightColor;
 	public Material lightMaterial;
+	public LayerMask colliderLayer;
+	public Material shadowMaterial;
 	[System.Serializable]
 	public struct MeshRenderInfo {
 		public MeshFilter filter;
@@ -21,11 +24,19 @@ public class PointLight : MonoBehaviour {
 				var renderer = go.AddComponent<MeshRenderer>();
 				renderer.material = material;
 				mesh = new Mesh();
+				mesh.MarkDynamic();
 				filter.mesh = mesh;
 			}
 		}
     }
-	public MeshRenderInfo light = new MeshRenderInfo();
+	public new MeshRenderInfo light = new MeshRenderInfo();
+	public MeshRenderInfo shadow = new MeshRenderInfo();
+	public int shadowRayCount;
+	public Vector2 Position {
+		get {
+			return transform.position;
+		}
+	}
 	void Update() {
 		light.Setup(this, "LightMesh", lightMaterial);
 		if(triangleCount < 3) {
@@ -38,8 +49,7 @@ public class PointLight : MonoBehaviour {
 		float currentAngle = 0;
 		for(int i = 0; i < triangleCount + 1; i++) {
 			currentAngle += stepAngle;
-			Vector2 dir = new Vector2(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle));
-			dir *= radius;
+			Vector2 dir = AngleToNormVec(currentAngle) * radius;
 			Vector3 dirV3 = new Vector3(dir.x, dir.y, 0);
 			Vector3 vertex = dirV3;
 			vertices[i + 1] = vertex;
@@ -62,5 +72,57 @@ public class PointLight : MonoBehaviour {
 		light.mesh.RecalculateNormals();
 		light.filter.mesh = light.mesh;
 
+		UpdateShadowMesh();
+	}
+	Vector2 AngleToNormVec(float angle) {
+		return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+	} 
+	void UpdateShadowMesh() {
+		shadow.Setup(this, "ShadowMesh", shadowMaterial);
+		shadow.mesh.Clear();
+		Vector2? lastHit = null;
+		Vector2 lastStart = Vector2.zero;
+		List<Vector3> vertices = new List<Vector3>();
+		List<int> triangles = new List<int>();
+		List<Color> colors = new List<Color>();
+		float stepAngle = Mathf.PI * 2 / shadowRayCount;
+		float currentAngle = 0;
+		Action<Vector2> addVertex = v2 => {
+			Vector3 vertex = new Vector3(v2.x, v2.y, transform.position.z);
+			vertex = transform.InverseTransformPoint(vertex);
+			vertices.Add(vertex);
+		};
+		for(int i = 0; i < shadowRayCount; i++) {
+			currentAngle += stepAngle;
+			Vector2 dir = -AngleToNormVec(currentAngle) * radius;
+			Vector2 start = Position - dir;
+			var hit = Physics2D.Raycast(start, dir, dir.magnitude, colliderLayer);
+			if(hit) {
+				addVertex(hit.point);
+				colors.Add(Color.black);
+				addVertex(start);
+				colors.Add(Color.black);
+				if(lastHit != null) {
+					int lastOrigin = vertices.Count - 4;
+					triangles.Add(lastOrigin + 0);
+					triangles.Add(lastOrigin + 3);
+					triangles.Add(lastOrigin + 1);
+					triangles.Add(lastOrigin + 0);
+					triangles.Add(lastOrigin + 2);
+					triangles.Add(lastOrigin + 3);
+				}
+				lastHit = hit.point;
+				lastStart = start;
+			}
+			else {
+				lastHit = null;
+			}
+		}
+		if(vertices.Count > 2) {
+			shadow.mesh.SetVertices(vertices);
+			shadow.mesh.SetTriangles(triangles, 0);
+			shadow.mesh.SetColors(colors);
+			shadow.mesh.RecalculateNormals();
+		}
 	}
 }
---
+++ Assets/Script/ShadowMesh.shader
@@ -0,0 +1,60 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+ Shader "2D/Light/ShadowMesh"
+ {  
+     Properties
+     {
+     }
+     SubShader
+     {
+         Tags 
+         { 
+             "LightMode" = "Light" 
+             "Queue" = "Geometry" 
+         }
+ 
+         Pass
+         {
+             ZWrite Off
+             ColorMask RGBA
+             Blend Off
+  
+             CGPROGRAM
+             #pragma vertex vert
+             #pragma fragment frag
+             #pragma multi_compile DUMMY PIXELSNAP_ON
+             #include "UnityCG.cginc"
+  
+ 
+             struct appdata_t
+			 {
+				float4 vertex   : POSITION;
+                float4 color    : COLOR;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct v2f
+			{
+				float4 vertex   : SV_POSITION;
+                float4 color    : COLOR;
+				UNITY_VERTEX_OUTPUT_STEREO
+			};
+  
+             v2f vert(appdata_t v)
+             {
+                 v2f o;
+     
+                 o.vertex = UnityObjectToClipPos(v.vertex);
+				 o.color = v.color;
+                 return o;
+             }
+                                                     
+             float4 frag(v2f IN) : COLOR
+             {
+                 return IN.color;
+             }
+ 
+             ENDCG
+         }
+     }
+ }
